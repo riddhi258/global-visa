899a419ac1e3177ec8abcf4135fcfa43b76f1bdf
USER-28 <USER-28@growmore.local>
2025-11-28 07:24:23 +0530

    project completed

--- index.php
+++ index.php
@@ -320,7 +320,7 @@ if (isset($_POST['submit'])) {
 
     if (empty($error)) {
         // Database connection
-        $con = new mysqli("mysql", "root", "root", "growmore");
+        $con = new mysqli("localhost", "root", "", "growmore");
         if ($con->connect_error) {
             die("Connection failed: " . $con->connect_error);
         }

9dbebe6377ad13c11f485f15b8366d4165b44f4d
USER-28 <USER-28@growmore.local>
2025-11-29 10:43:14 +0530

    project completed

--- index.php
+++ index.php
@@ -318,7 +318,7 @@ if (isset($_POST['submit'])) {
     //     }
     // }
 
-    $servername = "dpg-c1tn7wxyz12345-mysql.render.com"; // change from 127.0.0.1
+    $servername = "mysql"; // change from 127.0.0.1
     $username = "root";
     $password = "root@123";
     $dbname = "growmore";
